996b9418415a1b78130550648d498e31dfe1f6fb
Julio Bermudez <jbermudezfc90@gmail.com>
2020-06-05 16:52:21 -0400

    add upload image

--- src/components/CreatePostForm/CreatePostForm.jsx
+++ src/components/CreatePostForm/CreatePostForm.jsx
@@ -10,25 +10,37 @@ export default function CreatePostForm({ location }) {
     const [title, setTitle] = useState('');
     const [text, setText] = useState('');
     const [published, setPublished] = useState(true);
+    const [image, setImage] = useState('');
+    const [imagePreview, setImagePreview] = useState('');
+    const [errors, setErrors] = useState([]);
     const history = useHistory();
 
     const handleSubmit = async (e) => {
         try {
             e.preventDefault();
-            const formData = {
+            const postData = {
                 title,
                 text,
                 published,
+                image,
             };
+
+            console.log(postData);
             const request = await fetch('http://localhost:4000/api/post/create', {
                 method: 'post',
                 headers: { 'Content-Type': 'application/json', Authorization: authHeader() },
                 mode: 'cors',
-                body: JSON.stringify(formData),
+                body: JSON.stringify(postData),
             });
             const response = await request.json();
-            console.log(response);
-            history.push('/home');
+            if (request.status === 400) {
+                setErrors(response);
+                window.scrollTo(0, 0);
+                return;
+            } else {
+                console.log(response);
+                history.push('/home');
+            }
         } catch (error) {
             console.log(error);
         }
@@ -36,46 +48,75 @@ export default function CreatePostForm({ location }) {
 
     const editorRef = useRef();
 
+    const handleFile = (e) => {
+        setImagePreview(URL.createObjectURL(e.target.files[0]));
+        const reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0]);
+        reader.onload = () => {
+            setImage(reader.result);
+        };
+        console.log(reader);
+    };
     return (
-        <form
-            className="create-form"
-            onSubmit={(e) => handleSubmit(e)}
-            encType="multipart/form-data"
-        >
-            <label htmlFor="title">Title: </label>
-            <input
-                type="text"
-                name="title"
-                required
-                value={location.state ? location.state.title : title}
-                onChange={(e) => setTitle(e.target.value)}
-            />
-            <label htmlFor="text">Content: </label>
-            <Editor
-                previewStyle="vertical"
-                initialEditType="markdown"
-                height="700px"
-                events={{ change: () => setText(editorRef.current.getInstance().getMarkdown()) }}
-                ref={editorRef}
-                initialValue={location.state ? location.state.text : ''}
-            />
-            <label htmlFor="image">Image: </label>
-            <input type="file" name="image" />
-            <div className="radio-btns">
-                <p>Publish?</p>
-                <div>
-                    <label htmlFor="published">Yes</label>
-                    <RadioInput value={'true'} handleChange={(e) => setPublished(e.target.value)} />
+        <>
+            <ul className="errors">
+                {errors.length > 0
+                    ? errors.map((error) => <li key={error.msg}>{error.msg}</li>)
+                    : ''}
+            </ul>
+            <form
+                className="create-form"
+                onSubmit={(e) => handleSubmit(e)}
+                encType="multipart/form-data"
+            >
+                <label htmlFor="title">Title: </label>
+                <input
+                    type="text"
+                    name="title"
+                    required
+                    value={location.state ? location.state.title : title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label htmlFor="text">Content: </label>
+                <Editor
+                    previewStyle="vertical"
+                    initialEditType="markdown"
+                    height="700px"
+                    events={{
+                        change: () => setText(editorRef.current.getInstance().getMarkdown()),
+                    }}
+                    ref={editorRef}
+                    initialValue={location.state ? location.state.text : ''}
+                />
+                <label htmlFor="image">Image: </label>
+                <input
+                    type="file"
+                    name="image"
+                    accept=".png, .jpg, .jpeg"
+                    onChange={(e) => handleFile(e)}
+                />
+                <div className="image-container">
+                    <img src={imagePreview} alt="" />
                 </div>
-                <div>
-                    <label htmlFor="published">No</label>
-                    <RadioInput
-                        value={'false'}
-                        handleChange={(e) => setPublished(e.target.value)}
-                    />
+                <div className="radio-btns">
+                    <p>Publish?</p>
+                    <div>
+                        <label htmlFor="published">Yes</label>
+                        <RadioInput
+                            value={'true'}
+                            handleChange={(e) => setPublished(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="published">No</label>
+                        <RadioInput
+                            value={'false'}
+                            handleChange={(e) => setPublished(e.target.value)}
+                        />
+                    </div>
                 </div>
-            </div>
-            <button>Create Post</button>
-        </form>
+                <button>Create Post</button>
+            </form>
+        </>
     );
 }
